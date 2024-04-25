996befc61f9355c10453bbc81e27a16beb0621dc
sascha1987 <sascha.fluor@gmx.net>
2024-04-25 21:22:46 +0200

    Refactoring testing setup Problems with Code Coverage (Mocha / Chai) Removed Chai and Mocha dependencies Added Jest as new testing framework Updated depencies in package.json New file jest.setup.js file for global settings Adjusted Github Actions workflow to run Jest tests and code coverage

--- test/view/TimeSheetView.test.js
+++ test/view/TimeSheetView.test.js
@@ -1,11 +1,8 @@
 import { JSDOM } from "jsdom";
-import { expect } from "chai";
-import { describe, it, beforeEach, afterEach } from "mocha";
-import {
-  calculateDaysForMonth,
-  updateDOMWithDays,
-  calculateWorkingHours,
-} from "../../src/view/TimeSheetView.js";
+import { TextEncoder, TextDecoder } from "util";
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+import { calculateDaysForMonth, updateDOMWithDays, calculateWorkingHours } from "../../src/view/TimeSheetView.js";
 
 let dom, document;
 
@@ -21,58 +18,57 @@ afterEach(() => {
   delete global.document;
 });
 
-describe("updateDOMWithDays", function () {
-  it("should update the DOM based on the given year and month", function () {
+describe("updateDOMWithDays", () => {
+  test("should update the DOM based on the given year and month", () => {
     const year = 2020;
     const month = 0;
 
     updateDOMWithDays.call({ document }, year, month);
     const tableBody = document.querySelector("#timeSheet tbody");
 
-    expect(tableBody.rows.length).to.equal(31);
+    expect(tableBody.rows.length).toBe(31);
   });
 });
 
-describe("calculateDaysForMonth", function () {
-  it("should return 31 days for January 2020", function () {
+describe("calculateDaysForMonth", () => {
+  test("should return 31 days for January 2020", () => {
     const result = calculateDaysForMonth(2020, 0);
-    expect(result).to.have.lengthOf(31);
-  });
-  it("should return 29 days for February 2020 (leap year)", function () {
-    const result = calculateDaysForMonth(2020, 1);
-    expect(result).to.have.lengthOf(29);
+    expect(result).toHaveLength(31);
   });
 
-  it("should format dates corrctly for March 2021", function () {
+  test("should return 29 days for February 2020 (leap year)", () => {
+    const result = calculateDaysForMonth(2020, 1);
+    expect(result).toHaveLength(29);
+  });
+
+  test("should format dates correctly for March 2021", () => {
     const result = calculateDaysForMonth(2021, 2);
-    expect(result[0].date).to.equal("01.03.2021");
-    expect(result[30].date).to.equal("31.03.2021");
+    expect(result[0].date).toBe("01.03.2021");
+    expect(result[30].date).toBe("31.03.2021");
   });
 });
 
 describe("calculateWorkingHours", () => {
   beforeEach(() => {
     document.body.innerHTML = `
-    <table id="timeSheet">
-    <tbody>
-      <tr>
-        <td><input type="time" class="time-start" value="08:00"></td>
-        <td><input type="time" class="time-end" value="12:00"></td>
-        <td><input type="time" class="time-start-1" value="13:00"></td>
-        <td><input type="time" class="time-end-1" value="18:00"></td>
-        <td><input type="text" class="hours-normal" readonly></td>
-        <td><input type="text" class="overtime" readonly></td>
-      </tr>
-    </tbody>
-  </table>
-`;
+      <table id="timeSheet">
+      <tbody>
+        <tr>
+          <td><input type="time" class="time-start" value="08:00"></td>
+          <td><input type="time" class="time-end" value="12:00"></td>
+          <td><input type="time" class="time-start-1" value="13:00"></td>
+          <td><input type="time" class="time-end-1" value="18:00"></td>
+          <td><input type="text" class="hours-normal" readonly></td>
+          <td><input type="text" class="overtime" readonly></td>
+        </tr>
+      </tbody>
+      </table>
+    `;
   });
 
-  it("shoud calcualte total and overtime hours", () => {
+  test("should calculate total and overtime hours", () => {
     calculateWorkingHours();
-
     const hoursNormalInput = document.querySelector(".hours-normal");
-
-    expect(hoursNormalInput.value).to.equal("9.00"); // 8 hours normal Time + 1 hours overtime;
+    expect(hoursNormalInput.value).toBe("9.00"); // 8 hours normal time + 1 hour overtime
   });
 });
